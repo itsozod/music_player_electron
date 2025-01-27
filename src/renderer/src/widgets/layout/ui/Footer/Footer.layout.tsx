07b0c15fb416684ee6c041d503d70dbf1d646a95
Ozod <o.bukhorizoda@mail.ru>
2025-01-27 22:50:26 +0500

    chore: audio duration logic

--- src/renderer/src/widgets/layout/ui/Footer/Footer.layout.tsx
+++ src/renderer/src/widgets/layout/ui/Footer/Footer.layout.tsx
@@ -5,7 +5,16 @@ import { Pause, Play } from 'lucide-react'
 
 const Footer = () => {
   const { track } = useAudioStore()
-  const { audioRef, isPlaying, handlePause, handlePlay } = useAudioHandle()
+  const {
+    audioRef,
+    isPlaying,
+    handlePause,
+    handlePlay,
+    currentTime,
+    setCurrentTime,
+    duration,
+    setDuration
+  } = useAudioHandle()
 
   if (!track.length) return
 
@@ -13,7 +22,7 @@ const Footer = () => {
     <footer className="sticky w-[100%] left-0 bottom-2 mb-2 p-4 backdrop-blur-[15px] bg-[rgba(0,0,0,0.1)]">
       {track?.map((item) => {
         return (
-          <div className="flex items-center justify-between gap-2 w-full">
+          <div className="flex items-center justify-between gap-5 w-full">
             <div className="flex items-center gap-2">
               <img src={item?.img} alt="Song image" className="rounded-[50%]" />
               <div className="flex flex-col gap-1">
@@ -21,10 +30,26 @@ const Footer = () => {
                 <p>{item.artist}</p>
               </div>
             </div>
-            <audio ref={audioRef} src={item.preview} />
-            <Button onClick={isPlaying ? handlePause : handlePlay}>
-              {isPlaying ? <Pause /> : <Play />}
-            </Button>
+            <div className="flex flex-col flex-2">
+              <div className="flex justify-between gap-7 w-full">
+                <p>{currentTime}</p>
+                <p>{duration}</p>
+              </div>
+              <input min="0" max={duration} value={currentTime} type="range" />
+              <audio
+                ref={audioRef}
+                onLoadedData={(e) => {
+                  setDuration(e.currentTarget.duration?.toFixed(2))
+                }}
+                onTimeUpdate={(e) => {
+                  setCurrentTime(e.currentTarget.currentTime?.toFixed(2))
+                }}
+                src={item.preview}
+              ></audio>
+              <Button onClick={isPlaying ? handlePause : handlePlay}>
+                {isPlaying ? <Pause /> : <Play />}
+              </Button>
+            </div>
           </div>
         )
       })}
